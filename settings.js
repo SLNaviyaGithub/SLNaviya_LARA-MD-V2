fd78423392bcbf08df3e9a4c9590ddca4e7bc821
ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ <97608407+sadiyamin@users.noreply.github.com>
2025-01-11 16:08:36 +0530

    Lara-MD ⚘

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "vdQTTQAL#A5kMWXYQ1IJ2N32-wVpA_CpRzImtBkZEzPBBrXL2FFU", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "H7RzBL4A#0jFUp6p27ioK67xvkD8CUg2pxTNozUU9IrBeYjnTCI0", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
